4367082e5e3730e5aa130be1307207062e8a028f
wieczorek0807 <wieczorek0807@gmail.com>
2022-12-13 13:22:39 +0100

    data layer in progress

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -2,6 +2,6 @@ import 'package:dartz/dartz.dart';
 
 import '../error/failures.dart';
 
-abstract class UseCase<Type, Params> {
+abstract class UseCase<Type> {
   Future<Either<Failure, Type>> call();
 }
--- lib/features/animal_info/domain/usecases/GetRandomAnimalInfo.dart
+++
@@ -1,5 +0,0 @@
-// class GetRandomAnimalInfo impolements UseCase<AnimalInfo>{
-//   final AnimalInfoRepository repositoryl
-
-
-// }

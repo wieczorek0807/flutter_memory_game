d5d18d29983e81249e7f4aa93c2d015d98d5d697
MW <wieczorek0807@gmail.com>
2024-02-02 22:29:07 +0100

    Add files for GitHub Pages

--- android/app/src/main/kotlin/com/example/animals/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.animals
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/core/app_fonts.dart
+++
@@ -1,3 +0,0 @@
-abstract class AppFonts {
-  static const staatliches = 'Staatliches';
-}
